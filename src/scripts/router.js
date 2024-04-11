df40ff5f05666318fc6c35538ca206e0c4dba2b9
SonMyeongJin <smj2802@naver.com>
2024-04-11 21:29:09 +0900

    Home page basic setting

--- src/scripts/router.js
+++ src/scripts/router.js
@@ -2,12 +2,14 @@ import Login from "@/Pages/Login.vue";
 import {createRouter, createWebHistory} from "vue-router";
 import PostCreate from "@/Pages/PostCreate.vue";
 import PostList from "@/Pages/PostList.vue";
+import Home from "@/Pages/Home.vue";
 
 const routes = [
-    //{ path: "/", component: Home },
+    { path: "/", component: Home },
     { path : "/post-list", component: PostList},
     { path: "/create-post", component: PostCreate },
-    { path: "/login" , component: Login }
+    { path: "/login" , component: Login },
+
 ];
 
 const router = createRouter({
